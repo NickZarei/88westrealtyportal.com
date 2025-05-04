7791f808d5488f361dc9a1ff24f5a3fd6ad0b6e9
NickZarei <nicknimaz@gmail.com>
2025-05-04 15:15:16 -0700

    ✅ Tailwind styling and config fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./app/**/*.{ts,tsx,js,jsx}",
-      "./components/**/*.{ts,tsx,js,jsx}",
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
     },
     plugins: [],
-  };
+  }
   
